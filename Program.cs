80b752a56cfc412bad5b55825ce872dc0386c082
Jean-Marc Chapuzot <jean-marc.chapuzot@t-online.de>
2021-04-26 12:03:06 +0200

    2nd Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Playground
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello devel  World!");
+            Console.WriteLine("Hello devel 2 World!");
         }
     }
 }
