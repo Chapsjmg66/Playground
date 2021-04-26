6ea7ee632a3c6e0098fbd5f98e55438413fb6226
Jean-Marc Chapuzot <jean-marc.chapuzot@t-online.de>
2021-04-26 12:11:43 +0200

    Devel 2 branch commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Playground
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello devel 2 World!");
+            Console.WriteLine("Hello from branch devel 2!");
         }
     }
 }
